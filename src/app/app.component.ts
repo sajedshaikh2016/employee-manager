25ef69d9ad672f37d840079703fd7d7d312243d6
Sajed Shaikh <sajed.shaikh@hotmail.com>
2022-09-11 19:47:43 +0530

    App service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AppService } from './app.service';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'employee-manager';
+export class AppComponent implements OnInit {
+
+  public employees!: Employee[];
+
+  constructor(private _appService: AppService) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this._appService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error.message);
+      }
+    );
+  }
+
 }
