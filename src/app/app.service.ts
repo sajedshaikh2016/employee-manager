14a1454eecf0423088fb677562b0ad20ba864820
Sajed Shaikh <sajed.shaikh@hotmail.com>
2022-09-17 12:46:14 +0530

    Static UI update by using twitter bootstrap

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { Employee } from './employee';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
